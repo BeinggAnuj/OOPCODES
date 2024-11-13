a85718625d9c3cdc7321028b476fc588a62e62e6
Anuj Gholve <161754628+BeinggAnuj@users.noreply.github.com>
2024-11-14 01:25:23 +0530

    Add files via upload

--- Practical4.cpp
+++ Practical4.cpp
@@ -1,57 +1,86 @@
-#include<iostream>
-#include<fstream>
+#include <iostream>
+#include <fstream>
 using namespace std;
-class Employee              // declaring class employee
-{
- string Name;
- int ID;
- double salary;
- public:
- void accept()
- {
- cout<<"\n Name : ";
- cin.ignore();
-getline(cin,Name);
- cout<<"\n Id : ";
- cin>>ID;
- cout<<"\n Salary : ";
- cin>>salary;
- }
- void display()
- {
- cout<<"\n Name : "<<Name;
- cout<<"\n Id : "<<ID;
- cout<<"\n Salary : "<<salary<<endl;
- }
+
+class Employee {
+    string Name;
+    int ID;
+    double salary;
+
+public:
+    void accept() {
+        cout << "\nName: ";
+        cin.ignore();
+        getline(cin, Name);
+        cout << "ID: ";
+        cin >> ID;
+        cout << "Salary: ";
+        cin >> salary;
+    }
+
+    void display() const {
+        cout << "\nName: " << Name;
+        cout << "\nID: " << ID;
+        cout << "\nSalary: " << salary << endl;
+    }
+
+    // Save employee data to file (in text format)
+    void saveToFile(ofstream &f) const {
+        f << Name << "\n" << ID << "\n" << salary << "\n";
+    }
+
+    // Load employee data from file (in text format)
+    void loadFromFile(ifstream &f) {
+        getline(f, Name);
+        f >> ID;
+        f >> salary;
+        f.ignore(); // Ignore the newline after reading the salary
+    }
 };
 
-int main()
-{
- Employee o[5];
- fstream f;
- int i,n;
+int main() {
+    Employee o[5];
+    ofstream outFile;
+    ifstream inFile;
+    int i, n;
 
- f.open("demo.txt",ios::out);
- cout<<"\n Enter the number of employees you want to store : ";
- cin>>n;
- for(i=0;i<n;i++)
- {
- cout<<"\n Enter information of Employee "<<i+1<<"\n";
- o[i].accept();
- f.write((char*)&o[i],sizeof o[i]);
- }
+    // Open file for writing (in text mode)
+    outFile.open("demo.txt");
+    if (!outFile) {
+        cout << "Error opening file for writing!";
+        return 1;
+    }
 
- f.close();
+    cout << "Enter the number of employees you want to store (max 5): ";
+    cin >> n;
+    if (n > 5) {
+        cout << "Number of employees exceeds the limit of 5!";
+        return 1;
+    }
 
- f.open("demo.txt",ios::in);
- cout<<"\n Information of Employees is as follows : \n";
- for(i=0;i<n;i++)
- {
- cout<<"\nEmployee "<<i+1<<"\n";
- f.write((char*)&o[i],sizeof o[i]);
- o[i].display();
- }
- f.close();
- 
- return 0;
+    for (i = 0; i < n; i++) {
+        cout << "\nEnter information of Employee " << i + 1 << "\n";
+        o[i].accept();
+        o[i].saveToFile(outFile);
+    }
+
+    outFile.close();
+
+    // Open file for reading (in text mode)
+    inFile.open("demo.txt");
+    if (!inFile) {
+        cout << "Error opening file for reading!";
+        return 1;
+    }
+
+    cout << "\nInformation of Employees is as follows:\n";
+    for (i = 0; i < n; i++) {
+        cout << "\nEmployee " << i + 1 << "\n";
+        o[i].loadFromFile(inFile);
+        o[i].display();
+    }
+
+    inFile.close();
+
+    return 0;
 }
